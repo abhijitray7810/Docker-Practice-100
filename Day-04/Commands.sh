77422a363abf414a159fa7d64cf12bac5630aca8
Abhijit RAY <royabhijits928@gmail.com>
2025-10-01 17:57:11 +0530

    Update Commands.sh
    Signed-off-by: Abhijit RAY <royabhijits928@gmail.com>

--- Day-04/Commands.sh
+++ Day-04/Commands.sh
@@ -1,1 +1,12 @@
-
+ssh banner@stapp03
+password banner- BigGr33n
+sudo su -
+docker ps 
+docker ps -a
+docker rm kke-container
+if not delete 
+docker stop kke-container
+then
+docker rm kke-container
+docker rm -f kke-container
+docker ps -a | grep kke-container
